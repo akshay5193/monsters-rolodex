dd5b9988a8c480fc744eda59cc1c58e2627c6b13
Akshay Nande <akshaytnande@gmail.com>
2020-01-02 16:17:10 -0600

    Updates

---
+++ precache-manifest.75870504532c485ef0c115a21ee0a228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a39e5dea9b26e5dadf2207e01aa4677",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "699bbc4a7650ca386687",
+    "url": "/monsters-rolodex/static/css/main.13920c42.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "699bbc4a7650ca386687",
+    "url": "/monsters-rolodex/static/js/main.1bb02d50.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
