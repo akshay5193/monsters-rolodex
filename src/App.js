383069200129faa57a150a5e259ff29b4e46aedb
Akshay Nande <akshaytnande@gmail.com>
2020-01-02 16:14:32 -0600

    basic react app with search feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Akshay!!!',
+      monsters: [],
+      searchField: ''
+    };
+    // this.handleChange = this.handleChange.bind(this);  ----> you don't need this line if you use ES6 arrow function declaration...
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(err => console.log('error in getting data'))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+        //   <header className="App-header">
+        //   <img src={logo} className="App-logo" alt="logo" />
+        //   <p>{this.state.string}</p>
+        //   <button onClick={() => this.setState({ string: "Hello Ramoli!!!" })}>Change Text</button>
+        // </header>
